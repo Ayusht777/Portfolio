af9c3ac1f985fd3b96c8dc7a7b89cfc9bdd309ba
Ayush Talesara <talesaraayush@gmail.com>
2025-07-13 21:39:13 +0530

    Refactor global styles and enhance Navbar component
    - Simplified `globals.css` by removing unused variables and consolidating color definitions.
    - Added a new `Logo` component for branding in the Navbar.
    - Updated `layout.js` to apply background color from global styles.
    - Improved Navbar structure with theme toggle functionality and responsive design adjustments.

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,8 @@ import "./globals.css"
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
+    <html lang="en"  suppressHydrationWarning>
+      <body className="bg-background">
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
---
+++ components/shared/Logo.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+export const Logo = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="16" cy="16" r="16" fill="url(#paint0_linear)" />
+    <defs>
+      <linearGradient
+        id="paint0_linear"
+        x1="0"
+        y1="0"
+        x2="32"
+        y2="32"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="oklch(0.6630 0.1919 38.3911)" />
+        <stop offset="1" stopColor=" oklch(0.6900 0.2000 23.9100)" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
